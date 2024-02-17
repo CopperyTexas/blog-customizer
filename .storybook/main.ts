cdab275ca49397687e6f6487322e2560925d1f65
CopperyTexas <dengleboff@yandex.ru>
2024-02-17 12:42:46 +0500

    изменения от яндекса

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-const path = require('path');
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -8,70 +7,7 @@ const config: StorybookConfig = {
 		'@storybook/addon-essentials',
 		'@storybook/addon-onboarding',
 		'@storybook/addon-interactions',
-		'@storybook/addon-styling-webpack',
-		{
-			name: '@storybook/addon-styling-webpack',
-
-			options: {
-				rules: [
-					{
-						test: /\.css$/,
-						sideEffects: true,
-						use: [
-							require.resolve('style-loader'),
-							{
-								loader: require.resolve('css-loader'),
-								options: {
-									// Want to add more CSS Modules options? Read more here: https://github.com/webpack-contrib/css-loader#modules
-									modules: {
-										auto: true,
-									},
-								},
-							},
-						],
-					},
-					{
-						test: /\.s[ac]ss$/,
-						sideEffects: true,
-						use: [
-							require.resolve('style-loader'),
-							{
-								loader: require.resolve('css-loader'),
-								options: {
-									// Want to add more CSS Modules options? Read more here: https://github.com/webpack-contrib/css-loader#modules
-									modules: {
-										auto: true,
-									},
-									importLoaders: 2,
-								},
-							},
-							require.resolve('resolve-url-loader'),
-							{
-								loader: require.resolve('sass-loader'),
-								options: {
-									// Want to add more Sass options? Read more here: https://webpack.js.org/loaders/sass-loader/#options
-									implementation: require.resolve('sass'),
-									sourceMap: true,
-									sassOptions: {},
-								},
-							},
-						],
-					},
-				],
-			},
-		},
 	],
-	webpackFinal: async (config) => {
-		if (config?.resolve?.alias) {
-			config.resolve.alias = {
-				fonts: path.resolve(__dirname, '..', './src/fonts'),
-				src: path.resolve(__dirname, '..', './src'),
-				components: path.resolve(__dirname, '..', './src/components'),
-			};
-		}
-
-		return config;
-	},
 	framework: {
 		name: '@storybook/react-webpack5',
 		options: {
@@ -80,15 +16,6 @@ const config: StorybookConfig = {
 			},
 		},
 	},
-	swc: () => ({
-		jsc: {
-			transform: {
-				react: {
-					runtime: 'automatic',
-				},
-			},
-		},
-	}),
 	docs: {
 		autodocs: 'tag',
 	},
